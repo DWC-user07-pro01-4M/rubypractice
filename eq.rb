7c00e5eb3d437c0906e0e0b5b495f4ab1206994b
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-19 01:00:53 +0000

    [F]今日の練習はここまで

---
+++ eq.rb
@@ -0,0 +1,13 @@
+puts 1 == 2
+puts 2 == 2
+puts 2 == 1 + 1
+
+puts 1 != 2
+puts 2 != 2
+
+puts "ruby" == "ruby"
+puts "ruby" != "ruby"
+puts "ruby" == "xxx"
+
+puts 2.even?
+puts 3.even?
--- gtlt.rb
+++ gtlt.rb
@@ -1,2 +1,5 @@
 puts 1 < 2
 puts 1 > 2
+
+puts 1 < 2
+puts 2 >= 2
