b6efe8b9d59635ef91581ec0a52710a9d787ad1b
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-23 11:23:52 +0000

    [Study]｜｜と　＆＆

---
+++ or_end.rb
@@ -0,0 +1,13 @@
+# または、「OR」＝　｜｜
+wallet = 100
+suica = 300
+if wallet >= 300 || suica >= 300
+  puts "コンビニでアイスを買っていこう"
+end
+
+# かつ、「AND」＝＆＆
+wallet = 500
+weather = "fine"
+if wallet >= 300 && weather == "fine"
+  puts "コンビニでアイスを買っていこう"
+end
