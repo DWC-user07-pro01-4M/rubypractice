721ea7bfc7da16ccfefa5a641e466582e66bd8c7
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-23 11:02:35 +0000

    [Study]条件を満たさない時の処理else

---
+++ else.rb
@@ -0,0 +1,15 @@
+# 川沿いを散歩しよう
+wallet = 100
+if wallet >= 300
+  puts "コンビニでアイスを買っていこう"
+else
+  puts "川沿いを散歩しよう"
+end
+
+# アイスを買いに行こう
+wallet = 500
+if wallet >= 300
+  puts "コンビニでアイスを買おう"
+else
+  puts "川沿いを散歩しよう"
+end
