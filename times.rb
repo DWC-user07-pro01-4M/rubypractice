4c7ee9b0ad14804b0956a3edd97d85e9ade4212a
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-03-24 02:07:15 +0000

    [Study]繰り返しの処理times

--- times.rb
+++ times.rb
@@ -2,3 +2,25 @@
 3.times do
   puts "カフェラテ"
 end
+
+# ブロックで複数行
+3.times do
+  puts "緑茶"
+  puts "お願いします"
+end
+puts "注文は以上です"
+
+# do end の代わり　{}
+3.times {
+  puts "紅茶"
+}
+
+# ブロックと{}
+3.times do puts "モカ" end
+3.times { puts "カフェラテ" }
+
+3.times do
+  puts "モカ"
+  puts "ください"
+end
+3.times { puts "チャイください" }
