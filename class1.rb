f50fb6b27742ed66e128b43d42123cbdb50b3c2c
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-01 03:56:54 +0000

    [Study]クラスを学ぶ

---
+++ class1.rb
@@ -0,0 +1,18 @@
+p 1.class
+p 0.2.class
+p 100.class
+
+p "文字列".class
+p "hello".class
+p "".class
+
+p [1,2,3].class
+p ["文字列","name"].class
+p [].class
+
+# falseがかえる
+p 1.even?
+# trueがかえる
+p 2.even?
+# エラーが出る
+p "カフェ".even?
