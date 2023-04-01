1feb4eb79a7b19dd1f11d594ab8435d470047342
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-04-01 03:44:13 +0000

    [Study]ローカル変数とスコープを学ぶ

--- variable1.rb
+++ variable1.rb
@@ -1,1 +1,20 @@
 # ローカル変数とスコープ
+# 変数の見える範囲と寿命のことを「スコープ」という
+def hello
+  text = "こんにちは"
+  p text
+end
+hello
+# エラーになる。理由:メソッドの外から、メソッド内の定義は見えないから
+def hello
+  text = "こんにちは"
+end
+hello
+p text
+
+# エラーになる。理由:メソッドの外に書いてある変数もメソッドの中では見えないから
+text = "こんにちは"
+def hello
+  p text
+end
+hello
